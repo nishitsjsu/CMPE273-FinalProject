992edf22919b5820a73a50b9fad1cad030dc3ba8
Samkit Sheth <54513654+samkitsheth95@users.noreply.github.com>
2020-03-12 14:23:42 -0700

    Update sqlpool.js

--- backend/sql/sqlpool.js
+++ backend/sql/sqlpool.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "twitter.c64ohfdyhtuh.us-east-2.rds.amazonaws.com",
+  host: "#",
   user: "admin",
   password: "cmpe273twitter",
   database: "innodb"
